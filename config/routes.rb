193547c26d97d010e635d015ec09f12b99f72d27
Ryoko Yamada <z18037ry@aiit.ac.jp>
2020-05-29 22:09:32 +0000

    Finish ch05

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,23 @@
 Rails.application.routes.draw do
+  #get 'users/new'
   root 'static_pages#home'
+  get  '/help',    to: 'static_pages#help'
+  get  '/about',   to: 'static_pages#about'
+  get  '/contact', to: 'static_pages#contact'
+  get  '/signup',  to: 'users#new'  
+  # => contact_path '/contact'
+  # => contact_url 'ドメイン名/contact'
+end
+
+#Rails.application.routes.draw do
+#  get 'users/new'
+#  root 'static_pages#home'
   # root 'application#hello'
-  get 'static_pages/home'
+#  get 'static_pages/home'
   # => StaticPages#home
-  get 'static_pages/help'
+#  get 'static_pages/help'
   # => StaticPages#help
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get 'static_pages/about'
-  get 'static_pages/contact'
-end
+#  get 'static_pages/about'
+#  get 'static_pages/contact'
+#end
