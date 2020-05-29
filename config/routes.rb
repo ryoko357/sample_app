1b2cfbedece85cc0d3372b671290a43f0eae4a34
Ryoko Yamada <z18037ry@aiit.ac.jp>
2020-05-29 22:50:45 +0000

    retake Finish ch05

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   get  '/help',    to: 'static_pages#help'
   get  '/about',   to: 'static_pages#about'
   get  '/contact', to: 'static_pages#contact'
-  get  '/signup',  to: 'users#new'  
+  get  '/signup',  to: 'users#new'
   # => contact_path '/contact'
   # => contact_url 'ドメイン名/contact'
 end
