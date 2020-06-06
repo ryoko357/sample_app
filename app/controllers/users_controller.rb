e2e0deb4aca785dc091f17c30afe739fd419ac61
Ryoko Yamada <z18037ry@aiit.ac.jp>
2020-06-06 04:55:30 +0000

    Finish ch07

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,42 @@
 class UsersController < ApplicationController
-  def new
+  # GET /users/:id
+  def show
+    # ローカル変数 show(controller)の外では使えない user = User.first
+    # インスタンス変数 viewの中では使える
+    @user = User.find(params[:id])
+    # グローバル変数 @@user = User.first
+    # debugger
+    
   end
+  
+  # GET /users/new
+  def new
+    @user = User.new
+  end
+  
+  # POST /users (+params)
+  def create
+    # (@user + given params).save
+    @user = User.new(user_params)
+    if @user.save
+      #success
+      flash[:success] = "Welcom to the Sample App!"
+      # GET "users/#{@user.id}"
+      redirect_to @user
+      # redirect_to user_path(@user)
+      # redirect_to user_path(@user.id)
+      # redirect_to user_path(1) => /users/1      
+    else
+      #failure
+      render 'new'
+    # => name, email, pass/confirmation
+    end
+  end
+  
+  private
+  
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+  
 end
